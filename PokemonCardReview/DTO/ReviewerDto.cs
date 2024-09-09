4dcc202c9346d5dd62354e05de2e9d952aa7f15b
dimit <dimit@DIMITRI>
2024-09-09 20:48:27 +0400

    Add project files.

---
+++ PokemonCardReview/DTO/ReviewerDto.cs
@@ -0,0 +1,11 @@
+﻿using PokemonCardReview.Models;
+
+namespace PokemonCardReview.DTO
+{
+    public class ReviewerDto
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
---
+++ PokemonCardReview/Interfaces/IReviewRepository.cs
@@ -0,0 +1,16 @@
+﻿using PokemonCardReview.Models;
+
+namespace PokemonCardReview.Interfaces
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int reviewId);
+        ICollection<Review> GetReviewsOfAPokemon(int pokeId);
+        bool ReviewExists(int reviewId);
+        bool CreateReview(Review review);
+        bool UpdateReview(Review review);
+        bool DeleteReviews(List<Review> reviews);
+        bool Save();
+    }
+}
